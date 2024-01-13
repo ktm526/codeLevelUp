240cef03e6f0860ddeffb6f9a99e9dffac3256ee
ktm526 <ktm0526@postech.ac.kr>
2024-01-14 02:11:57 +0900

    day 4 str / group anagrams

---
+++ python/stringManipulation/group_anagrams.py
@@ -0,0 +1,20 @@
+#https://leetcode.com/problems/group-anagrams/
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        sorted_strs = []
+        for str_ in strs:
+            sorted_strs.append(''.join(sorted(str_)))
+        #print(sorted_strs)
+        str_dict = defaultdict(list)
+        for i in range(len(sorted_strs)):
+            str_dict[sorted_strs[i]].append(i)
+        result = []
+        for i in range(len(str_dict.keys())):
+            #print(list(str_dict.keys())[0])
+            result.append([])
+            for num in str_dict[list(str_dict.keys())[i]]:
+                #print(num)
+                result[i].append(strs[num])
+        #print(result)
+        return result
